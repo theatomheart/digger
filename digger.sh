bf2c3833d00c1e5ca50a299b77a97e3f9905b519
theatomheart <theatomheart@pm.me>
2025-11-24 18:32:38 +0000

    script changes

--- digger.sh
+++ digger.sh
@@ -6,7 +6,6 @@ Outpath="$HOME/.digger"		# This is the path the output will be saved to.
 
 RecordTypes="any ns a mx srv txt x"
 
-Output="$(date +%Y_%b_%d).md"
 
 if [ ! -f $Domains ] ; then echo "The file $Domains is missing." && exit 1 ; fi
 
@@ -14,11 +13,12 @@ DigLines=$(cat $Domains)
 
 for Line in $DigLines
 do
+	mkdir -p "$Outpath/$Line"
+	Output="$Outpath/$Line/$(date +%Y_%b_%d).md"
+#	cd "$Outpath/$Line"
 	echo "# Dig report for $Line" >> $Output
 	for Type in $RecordTypes
 	do
-		mkdir -p "$Outpath/$Line"
-		cd "$Outpath/$Line"
 		echo "### Record type: $Type" >> $Output
 		dig $Type $Line +noall +answer >> "$Output"
 #		echo "=== End of record type: $Type ===" >> $Output
