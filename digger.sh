51943f97867f1f2d24d9efeca589e82b9d5b75f3
theatomheart <theatomheart@pm.me>
2025-11-24 18:35:27 +0000

    script fixes

--- digger.sh
+++ digger.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
+RecordTypes="any ns a mx srv txt x"
+
 Domains="$HOME/domains.txt"	# This is your file with a list of domain names.
 
 Outpath="$HOME/.digger"		# This is the path the output will be saved to.
-
-RecordTypes="any ns a mx srv txt x"
-
 
 if [ ! -f $Domains ] ; then echo "The file $Domains is missing." && exit 1 ; fi
 
@@ -15,12 +14,10 @@ for Line in $DigLines
 do
 	mkdir -p "$Outpath/$Line"
 	Output="$Outpath/$Line/$(date +%Y_%b_%d).md"
-#	cd "$Outpath/$Line"
 	echo "# Dig report for $Line" >> $Output
 	for Type in $RecordTypes
 	do
-		echo "### Record type: $Type" >> $Output
+		echo "" >> $Output ; echo "## Record type: $Type" >> $Output
 		dig $Type $Line +noall +answer >> "$Output"
-#		echo "=== End of record type: $Type ===" >> $Output
 	done
 done
